e65d2dcb66881cf44cf543a3ad657d52f87cacd1
yarikpo <yaroslavpopovich04@gmail.com>
2023-01-27 17:44:30 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Press me!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
